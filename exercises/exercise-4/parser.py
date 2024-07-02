14a0c0f0e66ea34077a1a23d908429108e19aa9c
Dylan Moore <100745383+dmoore3554@users.noreply.github.com>
2024-07-02 13:55:30 -0500

    Convert parser to class

--- exercises/exercise-4/parser.py
+++ exercises/exercise-4/parser.py
@@ -1,48 +1,52 @@
 from turtle import *
-import time
-t = Turtle()
 
-def select_pen(number):
-    t.pensize(number)
+class parser:
+    def __init__(self):
+        self.t = Turtle()
+        self.commands = {
+            "P" : self.select_pen,
+            "D" : self.pen_down,
+            "U" : self.pen_up,
+            "N" : self.draw_north,
+            "E" : self.draw_east,
+            "S" : self.draw_south,
+            "W" : self.draw_west
+        }
 
-def pen_down():
-    t.down()
+    def select_pen(self, number):
+        self.t.pensize(number)
 
-def pen_up():
-    t.up()
+    def pen_down(self, ):
+        self.t.down()
 
-def draw_north(distance):
-    t.setheading(90)
-    t.forward(distance * 100)
+    def pen_up(self, ):
+        self.t.up()
 
-def draw_east(distance):
-    t.setheading(0)
-    t.forward(distance * 100)
+    def draw_north(self, distance):
+        self.t.setheading(90)
+        self.t.forward(distance * 100)
 
-def draw_south(distance):
-    t.setheading(270)
-    t.forward(distance * 100)
+    def draw_east(self, distance):
+        self.t.setheading(0)
+        self.t.forward(distance * 100)
 
-def draw_west(distance):
-    t.setheading(180)
-    t.forward(distance * 100)
+    def draw_south(self, distance):
+        self.t.setheading(270)
+        self.t.forward(distance * 100)
 
-commands = {
-    "P" : select_pen,
-    "D" : pen_down,
-    "U" : pen_up,
-    "N" : draw_north,
-    "E" : draw_east,
-    "S" : draw_south,
-    "W" : draw_west
-}
+    def draw_west(self, distance):
+        self.t.setheading(180)
+        self.t.forward(distance * 100)
 
-with open("exercises/exercise-4/turtle-code.txt", "r") as file:
-    for line in file:
-        time.sleep(1)
-        command = line.split()
-        function = commands.get(command[0])
-        if len(command) > 1:
-            function(int(command[1]))
-        else:
-            function()
+    def run_program(self, file_path):
+        with open(file_path, "r") as file:
+            for line in file:
+                command = line.split()
+                function = self.commands.get(command[0])
+                if len(command) > 1:
+                    function(int(command[1]))
+                else:
+                    function()
+
+my_parser = parser()
+my_parser.run_program("exercises/exercise-4/turtle-code.txt")
